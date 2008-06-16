0f6cf6d411c6ca2154f704d4604d0e5123f211c9
srndic <nedim.sh@gmail.com>
2008-06-16 16:55:35 +0000

    - new class RSA (the main class in the project, handles key generation, encryption and decryption) - code beautification
    git-svn-id: svn://localhost/tmp/rsa/rsa/trunk@24 709ee21d-d648-492f-ad28-bc2e0e7058a1

--- source/RSA.h
+++ source/RSA.h
@@ -1,13 +1,40 @@
+/* ****************************************************************************
+ * 
+ * 				RSA.h
+ * 
+ * Author: Nedim Srndic
+ * Release date: 16th of June 2008
+ * 
+ * An implementation of the RSA public-key cryptography algorithm. 
+ * 
+ * RSA supports: 
+ * 
+ * 	- Message encryption 
+ * 	- Message decryption 
+ * 	- Public/private key-pair generation 
+ * 
+ * ****************************************************************************
+ */
+
 #ifndef RSA_H_
 #define RSA_H_
+
+#include <string>
+#include "Key.h"
 
 class RSA
 {
 	private:
 		
 	public:
-		const std::string &Encrypt(const std::string input, const Key &key);
-		const std::string &Decrypt(const std::string input, const Key &key);
+		/* Encrypts "message" using "key". */
+		const std::string &Encrypt(	const std::string message, 
+									const Key &key);
+		/* Decrypts "cyphertext" using "key". */
+		const std::string &Decrypt(	const std::string cyphertext, 
+									const Key &key);
+		/* Generates a public/private key-pair. The keys are retured by 
+		 * reference, in the respective arguments. */
 		void GenerateKeyPair(Key &privateKey, Key &publicKey);
 };
 
